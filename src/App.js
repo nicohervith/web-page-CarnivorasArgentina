cfa20aab1ce069c94c016037866edeb311d288d9
nicohervith <niko_hervith@hotmail.com>
2021-11-30 03:38:44 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import Home from './Components/pages/Home'
 import Items from "./Components/pages/Items";
 import Logo from "./Components/pages/Logo";
-import acuatica1 from './assets/acuatica1.jpg'
-import puelodic2019 from './assets/puelodic2019.jpg'
+
+
 
 
 function App() {
@@ -14,57 +14,7 @@ function App() {
     <div className="App">
       <NavBar />
       <hr />
-      <div className="capa-sup">
-        <figure>
-          <img src={acuatica1} className="ac1" alt="ac1" />
-          <div className="capa">
-            <p>
-              Plantas carnívoras acuáticas de Argentina en plena floración.
-              Corrientes.
-            </p>
-          </div>
-        </figure>
-          <div className="card-text">
-            <p className="card-text-p">
-              Las plantas carnívoras atraen, capturan y digieren a sus presas.
-              Son especies fascinantes que despiertan el interés por su belleza
-              y lo atractivo de su rareza. Esta atracción que despiertan es una
-              gran oportunidad para acercarnos a la naturaleza y entender más
-              sobre el mundo que nos rodea.
-            </p>
-
-            <p className="card-text-p">
-              En el Proyecto Carnívoras Argentina hacemos investigación,
-              divulgación y cultivo.
-            </p>
-          </div>
-        <div className="card-text">
-          <p className="card-text-p">
-            Investigamos para conocer más sobre la distribución y estados de
-            conservación de las especies nativas. Es decir, aquellas especies
-            que crecen en estado silvestre en Argentina.
-          </p>
-
-          <p className="card-text-p">
-            Divulgamos para dar a conocer las plantas que nos rodean, empatizar
-            con los ecosistemas y así entender la importancia de cuidar la
-            naturaleza, de la cual somos parte. Divulgamos mediante
-            publicaciones, cursos virtuales y capacitaciones presenciales.
-          </p>
-        </div>
-        <div className="capa-sup">
-          <figure>
-            <img src={puelodic2019} className="puelo" alt="puelo" />
-            <div className="capa">
-              <p>
-                Educación ambiental con guardaparques y turistas en el Parque
-                Nacional Lago Puelo, Chubut.
-              </p>
-            </div>
-          </figure>
-        </div>
-      </div>
-
+      <Home />
       <hr />
 
       <br />
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,11 +1,66 @@
 import React from 'react'
+import acuatica1 from '../../assets/acuatica1.jpg';
+import puelodic2019 from '../../assets/puelodic2019.jpg'
+import  Carousel  from '../Carousel';
 
  const Home = () => {
   return (
     <div>
-        <h1>Pagina de inicio</h1>
+      <Carousel/>
+      <div className="capa-ppal">
+        <div className="capa-sup">
+          <figure className="figure">
+            <img src={acuatica1} className="ac1" alt="ac1" />
+            <div className="capa">
+              <p>
+                Plantas carnívoras acuáticas de Argentina en plena floración.
+                Corrientes.
+              </p>
+            </div>
+          </figure>
+          <div className="card-text1">
+            <p className="card-text-p">
+              Las plantas carnívoras atraen, capturan y digieren a sus presas.
+              Son especies fascinantes que despiertan el interés por su belleza
+              y lo atractivo de su rareza. Esta atracción que despiertan es una
+              gran oportunidad para acercarnos a la naturaleza y entender más
+              sobre el mundo que nos rodea.
+            </p>
+
+            <p className="card-text-p">
+              En el Proyecto Carnívoras Argentina hacemos investigación,
+              divulgación y cultivo.
+            </p>
+          </div>
+          <div className="card-text2">
+            <p className="card-text-p">
+              Investigamos para conocer más sobre la distribución y estados de
+              conservación de las especies nativas. Es decir, aquellas especies
+              que crecen en estado silvestre en Argentina.
+            </p>
+
+            <p className="card-text-p">
+              Divulgamos para dar a conocer las plantas que nos rodean,
+              empatizar con los ecosistemas y así entender la importancia de
+              cuidar la naturaleza, de la cual somos parte. Divulgamos mediante
+              publicaciones, cursos virtuales y capacitaciones presenciales.
+            </p>
+          </div>
+          <div className="capa-sup">
+            <figure className="figure">
+              <img src={puelodic2019} className="puelo" alt="puelo" />
+              <div className="capa">
+                <p>
+                  Educación ambiental con guardaparques y turistas en el Parque
+                  Nacional Lago Puelo, Chubut.
+                </p>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
